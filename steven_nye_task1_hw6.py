d7c79990064a81e60505cf8f442c109112a12949
Steven R. Nye <stevennye@mail.weber.edu>
2016-11-12 21:57:55 -0700

    added more logic to module 1 in task 1

--- steven_nye_task1_hw6.py
+++ steven_nye_task1_hw6.py
@@ -24,6 +24,58 @@ def mod1(hr, ld, rd, cl, ml, li, lo, ri, ro, gs):
 
     if gs != 'P' or gs != 'N' or gs != 'D' or gs != 'R' or gs != 1 or gs != 2 or gs != 3:
         print("Invalid Record: Both doors stay closed.")
+        exit(1)
+
+    if gs != 'P':
+        print("Both doors stay closed.")
+        exit(1)
+
+    if ml != 1:
+        print("Both doors stay closed.")
+        exit(1)
+
+    #Check for dashboard switches
+    if rd == 1 and ld == 1:
+        print("Both doors open.")
+        exit(0)
+        
+    lOpen = False
+    rOpen = False
+
+    elif rd == 1:
+        rOpen = True
+        
+    elif ld == 1:
+        lOpen = True
+
+    #Check outside handles
+    if lo == 1:
+        lOpen = True
+
+    if ro == 1:
+        rOpen = True
+
+    #Check inside handles
+    if li == 1 and cl == 0:
+        lOpen = True
+
+    if ri == 1 and cl == 0:
+        rOpen = True
+
+    #Print results
+    if lOpen == True and rOpen == True:
+        print("Both doors open.")
+        exit(0)
+    elif lOpen == True and rOpen == False:
+        print("Left door is open.")
+        exit(0)
+    elif lOpen == False and rOpen == True:
+        print("Right door is open.")
+        exit(0)
+    else:
+        print("Both doors stay closed.")
+        exit(1)
+
 
 
 def main():
