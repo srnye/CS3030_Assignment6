d1637dec7b24495feeefb594f43125490c4a63d5
Steven R. Nye <stevennye@mail.weber.edu>
2016-11-12 21:38:53 -0700

    added string conversions

--- steven_nye_task1_hw6.py
+++ steven_nye_task1_hw6.py
@@ -11,15 +11,15 @@ Description: Minivan task
 """
 
 def mod1(hr, ld, rd, cl, ml, li, lo, ri, ro, gs):
-    print("Heading Record " + hr + ":")
-    print("Left dashboard switch (0 or 1): " + ld)
-    print("Right dashboard switch (0 or 1): " + rd)
-    print("Child lock switch (0 or 1): " + cl)
-    print("Master unlock switch (0 or 1): " + ml)
-    print("Left inside handle (0 or 1): " + li)
-    print("Left outside handle (0 or 1): " + lo)
-    print("Right inside handle (0 or 1): " + ri)
-    print("Right outside handle (0 or 1): " + ro)
+    print("Heading Record " + str(hr) + ":")
+    print("Left dashboard switch (0 or 1): " + str(ld))
+    print("Right dashboard switch (0 or 1): " + str(rd))
+    print("Child lock switch (0 or 1): " + str(cl))
+    print("Master unlock switch (0 or 1): " + str(ml))
+    print("Left inside handle (0 or 1): " + str(li))
+    print("Left outside handle (0 or 1): " + str(lo))
+    print("Right inside handle (0 or 1): " + str(ri))
+    print("Right outside handle (0 or 1): " + str(ro))
     print("Gear shift position (P, N, D, 1, 2, 3 or R): " + gs)
 
     if gs != 'P' or gs != 'N' or gs != 'D' or gs != 'R' or gs != 1 or gs != 2 or gs != 3:
@@ -27,8 +27,8 @@ def mod1(hr, ld, rd, cl, ml, li, lo, ri, ro, gs):
 
 
 def main():
-    
-    pass
+   mod1(1, 1, 0, 0, 1, 0, 1, 0, 0, 'E') 
+   pass
 
 if __name__ == "__main__":
     main()
