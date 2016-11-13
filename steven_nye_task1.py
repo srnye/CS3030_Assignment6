545d39d00f042b3e333ae43242c8a728c0899559
Steven R. Nye <stevennye@mail.weber.edu>
2016-11-12 20:41:48 -0700

    initialized task 1

---
+++ steven_nye_task1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+import sys
+
+"""
+steven_nye_task1.py
+Created by Steven R. Nye on Nov 12, 2016
+Email: stevennye@mail.weber.edu
+Description: 
+"""
+
+def main():
+    
+    pass
+
+if __name__ == "__main__":
+    main()
+    exit(0)
+
