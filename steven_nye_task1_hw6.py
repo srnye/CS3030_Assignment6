5593034f1cca3fdd59dda8e33474754e5d658fe5
Steven R. Nye <stevennye@mail.weber.edu>
2016-11-13 01:29:39 -0700

    finished task 1

--- steven_nye_task1_hw6.py
+++ steven_nye_task1_hw6.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 
 import sys
+import csv
 
 """
 steven_nye_task1_hw6.py
@@ -92,15 +93,36 @@ def mod1(hr, ld, rd, cl, ml, li, lo, ri, ro, gs):
         print("Both doors stay closed.")
         return
 
+def mod2(fileName):
+    """
+    Function that loops through lines in CSV file for minivan configurations
+    Args:
+        name of CSV file
+    Returns:
+        Nothing
+    """
+    rowNum = 1
+    with open(fileName, 'rt') as csvfile:
+        configreader = csv.reader(csvfile)
+        for row in configreader:
+            if str(row[0]) == 'H1':
+                continue
+            mod1(rowNum, int(row[1].lstrip()), int(row[2].lstrip()), int(row[3].lstrip()), int(row[4].lstrip()), int(row[5].lstrip()), int(row[6].lstrip()), int(row[7].lstrip()), int(row[8].lstrip()), str(row[9]).lstrip())
+            print("")
+            rowNum = rowNum + 1
 
-
-def main():
-    mod1(1, 0, 1, 0, 1, 0, 1, 0, 0, 'P')
-    mod1(2, 1, 0, 0, 1, 0, 1, 0, 0, 'D')
-    mod1(3, 1, 0, 0, 1, 0, 1, 0, 0, 'E') 
+def main(argv):
+    """
+    Main
+    Args:
+        CSV file
+    Returns:
+        Nothing
+    """
+    mod2(str(sys.argv[1]))
     pass
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1])
     exit(0)
 
